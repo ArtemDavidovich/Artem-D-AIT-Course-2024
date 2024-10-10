3b76eed0f492406e3ebfe359b770bb0a59d78a6a
ArtemDavidovich <chumazek@gmail.com>
2024-10-10 08:25:11 +0200

    10-Oct-2024 Course

--- lesson_20/code/homework_20/min_max/MinMax.java
+++ lesson_20/code/homework_20/min_max/MinMax.java
@@ -2,51 +2,50 @@ package homework_20.min_max;
 
 //Задание 3. Напишите приложение, которое выводит минимальные/максимальные числовые значения примитивных типов: byte, short, char, int, long, float, double Типы как объекты String должны браться из аргументов основной функции main. Если в аргументах нет какого-либо типа (не указан тип), приложение должно вывести максимальное/минимальное значения 7 типов (byte, int, short, long, char, float, double). Если аргументы содержат неправильный тип, приложение должно вывести сообщение: Неверный тип.
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MinMax {
 
     public static void main(String[] args) {
 
-        String[] str = {"byte", "short", "char", "int", "long", "float", "double"};
+        //defining all correct seven types and putting them into a set
+        String[] types = {"byte", "short", "char", "int", "long", "float", "double"};
+        Set<String> typesSet = new HashSet<>(Arrays.asList(types));
+        //The Arrays.asList() method converts the array into a list. The HashSet<>(...) constructor converts the list into a set (Set<String>). A set is a collection that does not allow duplicates. Each element in the set is unique.
 
-        String[] temp = new String[str.length]; //creating new array for elements keeping
+        Set<String> finalTypes = new HashSet<>(); //creating a new set for types that are in the arguments and are correct
 
-        //looking through the array and defining if they were printed in the arguments
-        for (int i = 0; i < str.length; i++) {
-            if (args.length > i) {
-                String res = (i < args.length) ? args[i] : "";;
-                switch (res) {
-                    case "byte" -> temp[i] = "byte: min = " + Byte.MIN_VALUE + ", max = " + Byte.MAX_VALUE;
-                    case "short" -> temp[i] = "short: min = " + Short.MIN_VALUE + ", max = " + Short.MAX_VALUE;
-                    case "char" -> temp[i] = "char: min = " + (int) Character.MIN_VALUE + ", max = " + (int) Character.MAX_VALUE;
-                    case "int" -> temp[i] = "int: min = " + Integer.MIN_VALUE + ", max = " + Integer.MAX_VALUE;
-                    case "long" -> temp[i] = "long: min = " + Long.MIN_VALUE + ", max = " + Long.MAX_VALUE;
-                    case "float" -> temp[i] = "float: min = " + Float.MIN_VALUE + ", max = " + Float.MAX_VALUE;
-                    case "double" -> temp[i] = "double: min = " + Double.MIN_VALUE + ", max = " + Double.MAX_VALUE;
-                    default -> temp[i] = "Wrong type: " + res;
-                }
+        for(String arg : args){
+            if(typesSet.contains(arg)){
+                finalTypes.add(arg); //if the type is correct, we add it to the final set
+            } else {
+                System.out.println("Wrong type: " + arg); //if the type is incorrect, we print a "wrong" message
             }
         }
 
-        // Выводим результаты
-        for (String print : temp) {
-            System.out.println(print);
+        for(String missing : typesSet){
+            if (!finalTypes.contains(missing)){
+                finalTypes.add(missing); //adding the missing types to the final set
+            }
         }
 
+        //printing the minimum amd maximum values for our final types
+        for (String res : finalTypes) {
+                switch (res) {
+                    case "byte" -> System.out.println("byte: min = " + Byte.MIN_VALUE + ", max = " + Byte.MAX_VALUE);
+                    case "short" -> System.out.println("short: min = " + Short.MIN_VALUE + ", max = " + Short.MAX_VALUE);
+                    case "char" -> System.out.println("char: min = " + (int) Character.MIN_VALUE + ", max = " + (int) Character.MAX_VALUE);
+                    case "int" -> System.out.println("int: min = " + Integer.MIN_VALUE + ", max = " + Integer.MAX_VALUE);
+                    case "long" -> System.out.println("long: min = " + Long.MIN_VALUE + ", max = " + Long.MAX_VALUE);
+                    case "float" -> System.out.println("float: min = " + Float.MIN_VALUE + ", max = " + Float.MAX_VALUE);
+                    case "double" -> System.out.println("double: min = " + Double.MIN_VALUE + ", max = " + Double.MAX_VALUE);
+                }
+
+        }
 
     }//end of main
 
-    //else {
-    //                // if the type is missing we are printing it anyway
-    //                temp[i] = str[i];
-    //                switch (str[i]) {
-    //                    case "byte" -> temp[i] += " min = " + Byte.MIN_VALUE + ", max = " + Byte.MAX_VALUE;
-    //                    case "short" -> temp[i] += " min = " + Short.MIN_VALUE + ", max = " + Short.MAX_VALUE;
-    //                    case "char" -> temp[i] += " min = " + (int) Character.MIN_VALUE + ", max = " + (int) Character.MAX_VALUE;
-    //                    case "int" -> temp[i] += " min = " + Integer.MIN_VALUE + ", max = " + Integer.MAX_VALUE;
-    //                    case "long" -> temp[i] += " min = " + Long.MIN_VALUE + ", max = " + Long.MAX_VALUE;
-    //                    case "float" -> temp[i] += " min = " + Float.MIN_VALUE + ", max = " + Float.MAX_VALUE;
-    //                    case "double" -> temp[i] += " min = " + Double.MIN_VALUE + ", max = " + Double.MAX_VALUE;
-    //                }
-    //            }
-
 }//end of class
+
