713692bce4eb88bd2d250784fbc342d6c7342c58
ArtemDavidovich <chumazek@gmail.com>
2024-11-11 13:05:05 +0100

    11-Nov-2024 Course

---
+++ lesson_43/code/classwork_43/stream_string/CityStream.java
@@ -0,0 +1,34 @@
+package classwork_43.stream_string;
+
+//Возьмите список городов, отфильтруйте только те названия, которые начинаются с заданной буквы,
+//преобразуйте их в верхний регистр и выведите в алфавитном порядке.
+//"Augsburg", "Munich", "berlin", "Heidelberg", "Worms", "Frankfurt am Main", "Bremen", "Bonn", "Braunschweig"
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CityStream {
+
+    public static void main(String[] args) {
+
+        List<String> cityList = new ArrayList<>(Arrays.asList("Augsburg", "Munich", "berlin", "Heidelberg", "Worms", "Frankfurt am Main", "Bremen", "Bonn", "Braunschweig"));
+
+        char firstLetter = 'b';
+
+        List<String> result = filterCities(cityList, firstLetter);
+
+        result.forEach(System.out::println);
+
+    }//end of main
+
+    private static List<String> filterCities(List<String> cityList, char firstLetter) {
+        return cityList.stream()
+                .filter(city -> city.toLowerCase().startsWith(String.valueOf(firstLetter))) //filter by 'b'
+                .map(String::toUpperCase) //converting to Upper Case
+                .sorted() //sorting by alphabet
+                .toList() //putting into list
+                .reversed();
+    }
+
+}//end of class
